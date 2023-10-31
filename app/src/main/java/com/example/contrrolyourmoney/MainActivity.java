de8d2f3879ca8400bc06f0331c1d14a6fb0f46d3
Hamzaelmalem <hamzamaalem387@gmail.com>
2023-10-31 17:44:43 +0100

    first commit

--- app/src/main/java/com/example/contrrolyourmoney/MainActivity.java
+++ app/src/main/java/com/example/contrrolyourmoney/MainActivity.java
@@ -32,48 +32,9 @@ public class MainActivity extends AppCompatActivity {
         settingText = findViewById(R.id.settingText);
         homeText.setTextColor(Color.parseColor("#FFFFFFFF"));
         homeIcon.setBackgroundResource(R.drawable.home_activated);
-        // get all the fragment.
-        Fragment homeFragment = new HomeFragment();
-        Fragment searchFragment = new SearchFragment();
-        Fragment listFragment = new ListFragment();
-        Fragment settingFragment = new SettingFragment();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame, homeFragment);
-        fragmentTransaction.commit();
-
-
-        homeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setButtonAction(homeIcon, searchIcon, listIcon, settingIcon,R.drawable.home_activated, R.drawable.search, R.drawable.list, R.drawable.settings,homeText, searchText, listText,settingText);
-                setFragment(homeFragment);
-            }
-        });
-        searchIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setButtonAction(searchIcon, listIcon, homeIcon, settingIcon,R.drawable.search_activated, R.drawable.list, R.drawable.home, R.drawable.settings,searchText, listText, homeText,settingText);
-                setFragment(searchFragment);
-            }
-        });
-        settingIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setButtonAction(settingIcon, searchIcon, homeIcon, listIcon,R.drawable.settings_activated, R.drawable.search, R.drawable.home, R.drawable.list,settingText, searchText, homeText,listText);
-                setFragment(settingFragment);
-            }
-        });
-        listIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setButtonAction(listIcon, searchIcon, homeIcon, settingIcon,R.drawable.list_activated, R.drawable.search, R.drawable.home, R.drawable.settings,listText, searchText, homeText,settingText);
-                setFragment(listFragment);
-
-            }
-        });
     }
+
+
     public void setButtonAction(View viewEnabled, View viewDisabled1, View viewDisabled2, View viewDisabled3,  int backEnabled, int backDisabled1, int backDisabled2, int backDisabled3, TextView textEnabled, TextView textDisabled1, TextView textDisabled2, TextView textDisabled3){
         viewEnabled.setBackgroundResource(backEnabled);
         textEnabled.setTextColor(Color.parseColor("#FFFFFFFF"));
